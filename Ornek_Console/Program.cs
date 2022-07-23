80ea2f118f90f032470cf9af04a2a5b68485e8eb
Ramazan Yılmaz <ramazanyilmaz1771@hotmail.com>
2022-07-23 11:42:42 +0300

    Add project files.

---
+++ Ornek_Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kimlik kml = new Kimlik();
+            kml.AD = "Ramazan";
+            kml.SOYAD = "Yılmaz";
+            kml.MEMLEKET = "Gaziantep";
+            Console.WriteLine(kml.AD);
+            Console.WriteLine(kml.SOYAD);
+            Console.WriteLine(kml.MEMLEKET);
+            Console.WriteLine(kml.YAS);
+            Console.WriteLine(kml.CINSIYET);
+            Console.Read();
+        }
+    }
+}
